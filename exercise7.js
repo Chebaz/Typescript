5ecee9e8bfad40475684059efb62360dcc099018
Simone Checco <simone.checco1995@gmail.com>
2023-04-01 11:53:33 +0200

    esercizio7

--- exercise7.js
+++ exercise7.js
@@ -3,13 +3,23 @@
  * START: Follow the instructions below.
  */
 Object.defineProperty(exports, "__esModule", { value: true });
-var countryData = {
+var countryA = {
     name: "India",
     code: "IN",
     population: 1352642280,
+};
+var countryB = {
+    name: "Italy",
+    code: "IT",
+};
+var countryC = {
+    name: "Spain",
+    code: "ES",
+    population: 47450795,
 };
 var currencyData = {
     name: "Euro",
     code: "EUR",
     symbol: "€",
 };
+currencyData.code = "GGG";
