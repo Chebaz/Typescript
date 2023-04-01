ff39849be981ba8c6f67d6a7aebd7e586a2ababc
Simone Checco <simone.checco1995@gmail.com>
2023-04-01 11:25:06 +0200

    esercizio5

--- exercise4.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-/**
- * START: Follow the instructions below.
- */
-Object.defineProperty(exports, "__esModule", { value: true });
-var country1 = "Nigeria";
-var country2 = "Italy";
-var country3 = "China";
