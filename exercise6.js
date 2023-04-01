90ab72aa739868313048d25ae1642e4f2c37e42c
Simone Checco <simone.checco1995@gmail.com>
2023-04-01 11:39:30 +0200

    esercizio 6

---
+++ exercise6.js
@@ -0,0 +1,15 @@
+"use strict";
+/**
+ * START: Follow the instructions below.
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+var countryData = {
+    name: "India",
+    code: "IN",
+    population: 1352642280,
+};
+var currencyData = {
+    name: "Euro",
+    code: "EUR",
+    symbol: "€",
+};
