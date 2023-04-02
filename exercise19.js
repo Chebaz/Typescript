1b6fd7182b0f70439e5eef6e14dc52462ff03a4e
Simone Checco <simone.checco1995@gmail.com>
2023-04-02 22:58:25 +0200

    esercizio completo

---
+++ exercise19.js
@@ -0,0 +1,18 @@
+"use strict";
+/**
+ * START: Follow the instructions below.
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+var countryA = {
+    name: "Greece",
+    code: "GR",
+    languages: ["Greek"],
+};
+// Use the type alias syntax: type CountryWithStatistics = type;
+// Add a type annotation with the `CountryWithStatistics` type on
+// the variable `countryB`.
+var countryB = {
+    name: "China",
+    code: "CN",
+    population: 1412600000,
+};
